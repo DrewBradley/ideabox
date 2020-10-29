b29c38fb1a294601bcabb73bae9daf15bdef96c2
Cole Fiscus <coletfiscus@gmail.com>
2020-10-29 16:04:28 -0600

    Add favorite functionality and remove dummy card

--- src/main.js
+++ src/main.js
@@ -6,45 +6,47 @@ var bodyInput = document.querySelector(".body-input");
 var saveButton = document.querySelector(".save-button");
 var searchInput = document.querySelector(".search-input");
 var searchButton = document.querySelector(".search-button");
-var starButton = document.querySelector(".like");
-var deleteButton = document.querySelector(".delete");
-var commentButton = document.querySelector(".comment-button");
-var ideaGrid = document.querySelector(".idea-grid")
+var ideaGrid = document.querySelector(".idea-grid");
 
 var savedIdeas = [];
 
 // EVENT LISTENERS
 saveButton.addEventListener('click', saveIdea);
 bodyInput.addEventListener('keyup', enableSaveButton);
+ideaGrid.addEventListener("click", checkEventTarget);
 
 // FUNCTIONS
 function saveIdea() {
   if (titleInput.value && bodyInput.value) {
     var idea = new Idea(titleInput.value, bodyInput.value);
     savedIdeas.push(idea);
-  }
+  };
   titleInput.value = "";
   bodyInput.value = "";
   saveButton.disabled = true;
   showIdeas();
-  console.log('hot damn')
-}
+};
 
 function enableSaveButton() {
-  if (titleInput.value.length > 0 && bodyInput.value.length > 0){
+  if (titleInput.value.length > 0 && bodyInput.value.length > 0) {
     saveButton.disabled = false;
-  }else{
+  } else {
     saveButton.disabled = true;
-  }
-}
+  };
+};
 
 function showIdeas() {
   ideaGrid.innerHTML = "";
-  for (var i = 0; i < savedIdeas.length; i++){
+  for (var i = 0; i < savedIdeas.length; i++) {
+    if (savedIdeas[i].star === false) {
+      var star = "assets/star.svg";
+    } else if (savedIdeas[i].star === true) {
+      var star = "assets/star-active.svg";
+    };
     ideaGrid.innerHTML += `
       <article class="idea-container" id=${savedIdeas[i].id}>
         <span class="favorite-delete">
-          <img src="assets/star.svg" class="like" alt="Like this idea!">
+          <img src=${star} class="like" alt="Like this idea!">
           <img src="assets/delete.svg" class="delete" alt="Delete this idea!">
         </span>
         <p class="idea-title">${savedIdeas[i].title}</p>
@@ -55,24 +57,34 @@ function showIdeas() {
         </span>
       </article>
     `
-  }
-}
-ideaGrid.addEventListener("click", checkEventTarget);
-
-function checkEventTarget(event){
-  if(event.target.classList.contains("delete")){
-    deleteIdea(event);
-  } else if (event.target.className === "like"){
-    favoriteIdea(event);
-  }
+  };
 };
 
-function deleteIdea(event){
-  var id = parseInt(event.target.parentNode.parentNode.id);
-  for(var i = 0; i < savedIdeas.length; i++){
-    if(savedIdeas[i].id === id){
-      savedIdeas.splice(i, 1)
-    }
-  showIdeas();
-  }
+function checkEventTarget(event) {
+  var eventTarget = event.target;
+  if (eventTarget.classList.contains("delete")) {
+    deleteIdea(eventTarget);
+  } else if (eventTarget.classList.contains("like")) {
+    favoriteIdea(eventTarget);
+  };
+};
+
+function deleteIdea(target) {
+  var id = parseInt(target.parentNode.parentNode.id);
+    for (var i = 0; i < savedIdeas.length; i++) {
+      if (savedIdeas[i].id === id) {
+        savedIdeas.splice(i, 1);
+      };
+    };
+    showIdeas();
+};
+
+function favoriteIdea(target) {
+  var id = parseInt(target.parentNode.parentNode.id);
+    for (var i = 0; i < savedIdeas.length; i++) {
+      if (savedIdeas[i].id === id) {
+        savedIdeas[i].star = !savedIdeas[i].star;
+      };
+    };
+    showIdeas()
 };
